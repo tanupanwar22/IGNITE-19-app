4ee20442546a3aaaa08f11139907b3a510d86ea2
Asmanjas <asmanjas007@gmail.com>
2019-10-06 22:01:02 +0530

    minor updates

--- app/src/main/java/com/example/ignite19/ui/ContactUS/Fragment_ContactUs.java
+++ app/src/main/java/com/example/ignite19/ui/ContactUS/Fragment_ContactUs.java
@@ -21,7 +21,6 @@ public class Fragment_ContactUs extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
